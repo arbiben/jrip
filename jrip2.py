506ada4690fad6145e51069b42a225fb2c9474e1
Ben Arbib <ba2490@columbia.edu>
2018-04-29 02:33:01 -0400

    new jrip2 sending completely works

--- jrip2.py
+++ jrip2.py
@@ -11,18 +11,16 @@
 
 import socket, time, json, random
 import threading, sys, re
-import argparse, json, copy
+import json, copy, argparse
 
 lock = threading.Lock()   # lock variables from being accessed by more than one thread
-ack_window = {}           # holds ack records for eack host 
-ip_ack = {}               # every host is assigned an id
-max_packet_seq = 100      # amount of pings we send to other servers
-window = 6                # window size
+ack_window = {}           # holds ack records for eack host
 events = {}               # dict of events per host
-last_seq = {}             # dict of hosts who pinged me
+pinging_me = {}           # dict of hosts who pinged me
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
 
 # flags for user input
 parser = argparse.ArgumentParser()
@@ -54,120 +52,20 @@ cost_table["Data"]["Type"] = "JRIP"
 cost_table["Data"]["RIPTable"] = []
 
 for i in range(len(hosts)):
-    h,p,c = hosts[i].split(":")
+    h, p = hosts[i].split(":")
     temp = {}
-    
+
     check = re.search('[a-zA-Z]', h)
     if check is not None:
         h = socket.gethostbyname(h)
-    
+
     temp["neighbor"] = h+":"+p
-    temp["cost"] = c
+    temp["cost"] = 0
     cost_table["Data"]["RIPTable"].append(copy.deepcopy(temp))
 
 cost_json = json.dumps(cost_table)
 
-
-# change the window based on ACK received and resend new packets
-# TODO ADJUST WINDOW SIZE AFTER PASSING 95 PACKETS ACK
-def slide_window(hid):
-    temp_window = []
-    last = 0
-    with lock:
-        change = int(ack_window[hid][5])
-        if change == -1:
-            return
-        last = ack_window[hid][change][0]
-    
-    for _ in range(window-1):
-        last = last+1
-        temp_window.append([last, False])
-    
-    with lock:
-        # update the receive window
-        ack_window[hid] = copy.deepcopy(temp_window)
-        ack_window[hid].append(-1)
-        print("after swap {}".format(ack_window[hid]))
-
-# send packets from index to end of window
-def send_packets(hid):
-    ip, port = hid.split(":")
-    with lock:
-        did_change = ack_window[hid][5]
-
-    if did_change==-1:
-        did_change = 0
-    
-    for i in range(did_change,window-1):
-        rand = random.randint(0,100)
-        if rand >= loss_rate:
-            with lock:
-                cost_table["SEQ"] =  ack_window[hid][i][0]
-                sock.sendto(json.dumps(cost_table).encode(), (ip,int(port)))
-
-
-
-# thread that manages a specific connection
-# TODO manage time outs
-def neighbor_thread(d, host_id):
-    while True:
-        events[host_id].wait(1)
-        if not events[host_id].is_set():
-            send_packets(host_id)
-        else:
-            slide_window(host_id)
-            events[host_id].clear()
-
-        
-
-# binary search for index of packet in window
-def get_index_of_ack_num(target, arr, l, r):
-    if l>r:
-        return -1
-    mid = int((l+r)/2) 
-    if arr[mid][0] == target:
-        return mid
-    if arr[mid][0] > target:
-        return get_index_of_ack_num(target, arr, l, mid-1)
-
-    return get_index_of_ack_num(target, arr, mid+1, r)
-    
-
-# gets an ACK packege and updates window associated with it
-def handle_ack(addr, jrip_file):
-    ip = addr[0]+":"+str(addr[1])
-    ack_num = jrip_file["ACK"]-1
-    copy_list = ack_window[ip][:]
-    i = get_index_of_ack_num(ack_num, copy_list, 0, len(copy_list)-2)
-    if i != -1 and copy_list[i][1] is False:
-        with lock:
-            ack_window[ip][i] = [ack_window[ip][i][0], True]
-            ack_window[ip][5] = i
-            events[ip].set()
-            # print("{}".format(ack_window[ip]))
-        
-
-# gets data (ping) and responses with ACK if it comes in order
-def handle_data(addr, jrip_file):
-    seq_num = jrip_file["SEQ"]
-    ack_pack = {}
-    ack_pack["SEQ"] = -1
-    ack_pack["ACK"] = ack_num
-    ip, port = addr
-    hid = ip+":"+port
-    if hid not in last_seq:
-        last_seq[hid] = 0
-        
-    if last_seq[hid] == seq_num:
-        last_seq[hid] = last_seq[hid] + 1
-        ack_pack = {}
-        ack_pack["SEQ"] = -1
-        ack_pack["ACK"] = last_seq[hid]
-        ct = json.dumps(cost_table)
-        sock.sendto(ct.encode(), (ip, int(port)))
-
-# listens for all incoming packets and updates appropriate host handlers
-def listener_thread(d1,d2):
+def listener_thread():
     while True:
         data, addr = sock.recvfrom(4096)
         jrip_file = json.loads(data)
@@ -176,22 +74,131 @@ def listener_thread(d1,d2):
             t = threading.Thread(target=handle_ack, args=args)
             t.start()
         else:
-            t = threading.Thread(target=handle_data, args=args)
+            t = threading.Thread(target=handle_ping, args=args)
             t.start()
 
-        #print(data)
+def handle_ping(addr, jrip_file):
+    hid = str(addr[0])+":"+str(addr[1])
+    seq_num = jrip_file["SEQ"]
+    
+    with lock:
+        if hid not in pinging_me:
+            pinging_me[hid] = 0
+        
+        expecting = pinging_me[hid]
+        if expecting <= seq_num:
+            pinging_me[hid] = expecting + 1 if expecting==seq_num else expecting
+            cost_table["ACK"] = expecting+1
+            cost_table["SEQ"] = -1
+            sock.sendto(json.dumps(cost_table).encode(), (addr[0], int(addr[1])))
 
-# args is a dummy tuple
-args = (0,0)
-t = threading.Thread(target=listener_thread, args=args)
+
+def handle_ack(addr, jrip_file):
+    hid = str(addr[0])+":"+str(addr[1])
+    ack_num = jrip_file["ACK"] - 1
+    if ack_num < 100:
+        with lock:
+            win_copy = copy.deepcopy(ack_window[hid])
+        
+        win_copy = win_copy[:-1]
+
+        i = get_index_ack(ack_num, win_copy, 0, len(win_copy)-1)
+        if i != -1 and win_copy[i][1] is False:
+            win_copy[i] = [win_copy[i][0],True]
+            win_copy.append(i)
+            #print("changes have been made {}".format(win_copy))
+            with lock:
+                ack_window[hid] = copy.deepcopy(win_copy)
+
+            events[hid].set()
+
+def get_index_ack(target, arr, l, r):
+    if l>r:
+        return -1
+    
+    mid = int((l+r)/2)
+    if target == arr[mid][0]:
+        return mid
+    
+    if target > arr[mid][0]:
+        return get_index_ack(target,arr, l, mid-1)
+    
+    return get_index_ack(target, arr, mid+1, r)
+
+def send_packets(hid):
+    with lock:
+        win_copy = copy.deepcopy(ack_window[hid])
+        win_copy = win_copy[:-1]
+        last = 0 + ack_window[hid][-1:][0]
+        dic = dict(cost_table)
+
+    if len(win_copy) >= 1:
+        ip, port = hid.split(":")
+        change = 0 if last == -1 else last
+        #print("sending packets {}-{} to {}".format(change, change+4, hid))
+        for i in range(change, len(win_copy)):
+            if random.randint(0,100) >= loss_rate:
+                dic["SEQ"] = win_copy[i][0]
+                sock.sendto(json.dumps(dic).encode(), (ip, int(port)))
+            else:
+                with lock:
+                    ack_window[hid+"D"] = ack_window[hid+"D"] + 1
+            with lock:
+                ack_window[hid+"T"] = ack_window[hid+"T"] + 1
+
+def slide_window(hid):
+    with lock:
+        win_copy = copy.deepcopy(ack_window[hid])
+        win_copy = win_copy[:-1]
+        change = ack_window[hid][-1]
+    
+    if len(win_copy) > 0 and change > -1:
+
+        curr = win_copy[change][0]+1
+        win_copy = []
+        for i in range(curr, curr+5):
+            if i<100:
+                win_copy.append([i,False])
+    
+    win_copy.append(-1)
+    
+    with lock:
+        ack_window[hid] = copy.deepcopy(win_copy)
+
+
+def neighbor_thread(a, hid):
+    while True:
+        with lock:
+            #print("length of {} window is {}".format(hid, ack_window[hid]))
+            if len(ack_window[hid]) <= 1:
+                drop = ack_window[hid+"D"]
+                total = ack_window[hid+"T"]
+                rate = int(((total-drop)/total)*100)
+                print("Goodput Rate at {} {}/100".format(hid, rate))
+                exit()
+
+        
+        events[hid].wait(0.5)
+        if not events[hid].is_set():
+            send_packets(hid)
+
+        slide_window(hid)
+        events[hid].clear()
+        send_packets(hid)
+
+# call listenning thread
+t = threading.Thread(target=listener_thread)
 t.start()
 
 
 # create a thread for every host given in command line
 for k in cost_table["Data"]["RIPTable"]:
     with lock:
-        ack_window[k["neighbor"]] = [[0,False],[1,False],[2,False],[3,False],[4,False], -1]
-        events[k["neighbor"]] = threading.Event()
-    aargs = (0,k["neighbor"])
-    t = threading.Thread(target=neighbor_thread, args=aargs)
+        hid = str(k["neighbor"])
+        ack_window[hid] = [[0, False], [1, False],[2, False], [3, False], [4, False], -1]
+        ack_window[hid+"D"] = 0 # drop rate
+        ack_window[hid+"T"] = 0 # total sent
+        events[hid] = threading.Event()
+    args=(True, hid)
+    t = threading.Thread(target=neighbor_thread, args=args)
     t.start()
