f8fb64825ab0dce647fc9df7dc0c6f2e9a9a89aa
Ben Arbib <ba2490@columbia.edu>
2018-04-29 03:07:29 -0400

    small ad

--- jrip2.py
+++ jrip2.py
@@ -100,7 +100,7 @@ def handle_ack(addr, jrip_file):
     print(jrip_file)
     hid = str(addr[0])+":"+str(addr[1])
     ack_num = jrip_file["ACK"] - 1
-    if ack_num < 100:
+    if ack_num < 101:
         with lock:
             win_copy = copy.deepcopy(ack_window[hid])
         
