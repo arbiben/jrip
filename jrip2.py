eb9b95d2bf0aa42b0fed27185f73d380f8391e31
Ben Arbib <ba2490@columbia.edu>
2018-04-29 02:57:14 -0400

    ping

--- jrip2.py
+++ jrip2.py
@@ -89,7 +89,7 @@ def handle_ping(addr, jrip_file):
         expecting = pinging_me[hid]
         if expecting <= seq_num:
             pinging_me[hid] = expecting + 1 if int(expecting) == int(seq_num) else expecting
-            cost_table["ACK"] = expecting
+            cost_table["ACK"] = pinging_me[hid]
             cost_table["SEQ"] = -1
             print("pingign back {}".format(cost_table["ACK"]))
             sock.sendto(json.dumps(cost_table).encode(), (addr[0], int(addr[1])))
